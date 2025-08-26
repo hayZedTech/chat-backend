e14afcf549f429d5727d6c5a1f4f43e565d0d2e5
Azeez Ololade <ololadeazeez.m@gmail.com>
2025-08-26 23:10:43 +0100

    Initial commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ CORS setup for local + Render frontend
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://chat-frontend-flame-six.vercel.app" // ✅ add this
+  "hhttps://chat-frontend-wslj.vercel.app" // ✅ add this
 ];
 
 app.use(
